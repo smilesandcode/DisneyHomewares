9b3507fab92296115096efde73be7c6b3d86a8b4
smilesandcode <smilesandcode@gmail.com>
2017-08-28 18:19:55 -0400

    Finished forms, started order confirmation page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
+  // First image form.
   $("#waffle").click(function(event){
+    $(".confirm-order").fadeToggle();
+  });
+
+  // Second image form.
+  $("#ursula").click(function(event){
+    $(".confirm-order").fadeToggle();
+  });
+
+  // Third image form.
+  $("#stitch").click(function(event){
+    $(".confirm-order").fadeToggle();
+  });
+
+  // Fourth image form.
+  $("#pixar").click(function(event){
+    $(".confirm-order").fadeToggle();
+  });
+
+  // Fifth image form.
+  $("#mickey").click(function(event){
+    $(".confirm-order").fadeToggle();
+  });
+
+  // Sixth image form.
+  $("#poohBear").click(function(event){
     $(".confirm-order").fadeToggle();
   });
 });
